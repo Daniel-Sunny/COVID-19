2b6bb47afa3ba10c09b41c952a6e1bbfae6b4ea9
PSDL-security <haesunglee1994@psdl.co.kr>
2021-06-03 10:04:00 +0900

    use axios API for COVID-19 data

---
+++ README.md
@@ -0,0 +1,21 @@
+# todo list
+- option 안에 있는 글자 가운데 정렬
+
+
+
+
+
+# inatall list
+- chart library
+``` bash
+npm install  react-chartjs-2 --save
+```
+- axios api library
+``` bash
+npm install  axios --save
+```
+
+## develop note
+-  api call을 했을 때 console에서 Promise라고 나오는 경우
+   -   데이터를 다 받아오기도 전에 변수를 호출해서 문제가 생긴 것 
+   -   async/ await를 사용하게 되면 해결됨
--- src/components/Contents.js
+++ src/components/Contents.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+// useState : 데이터를 담아서 배열을 관리
+// useEffect : fetch api 사용
+
+import axios from 'axios'
 
 const Contents = () => {
+  useEffect(() => {
+    const fetchEvents = async () => {
+      const res = await axios.get("https://api.covid19api.com/total/dayone/country/kr")
+      console.log(res)
+    }
+
+    fetchEvents()
+  })
+
   return (
     <section>
-    <h2> 국내 코로나 현황</h2>
-    <div className="contents">
+      <h2> 국내 코로나 현황</h2>
+      <div className="contents">
 
-    </div>
-  </section>
+      </div>
+    </section>
   )
 }
 
